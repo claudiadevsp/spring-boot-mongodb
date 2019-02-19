0d4c483aa82ccd0aa8a22cc9ba3046b5de842380
Claudia <claudia.devsp@gmail.com>
2019-02-19 10:48:13 -0300

    implement api rest

--- src/main/java/com/css/mongo/domain/resources/UserResource.java
+++ src/main/java/com/css/mongo/domain/resources/UserResource.java
@@ -1,22 +1,22 @@
 package com.css.mongo.domain.resources;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.css.mongo.domain.User;
+import com.css.mongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
-		User mary = new User("1", "mary", "teste@gmail.com");
-		User alex = new User("2", "alex", "teste2@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(mary, alex));
+	public ResponseEntity<List<User>> findAll() {		
+		List<User> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 
