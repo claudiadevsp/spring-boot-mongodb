4fc0bb9f09194f91bb6488e98acbf2b7509f48e1
Claudia <claudia.devsp@gmail.com>
2019-02-19 16:12:45 -0300

    implements return DTO

--- src/main/java/com/css/mongo/domain/resources/UserResource.java
+++ src/main/java/com/css/mongo/domain/resources/UserResource.java
@@ -1,11 +1,14 @@
 package com.css.mongo.domain.resources;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.css.mongo.domain.User;
+import com.css.mongo.dto.UserDTO;
 import com.css.mongo.services.UserService;
 
 @RestController
@@ -15,9 +18,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {		
-		List<User> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<UserDTO>> findAll() {		
+		List<User> list = service.findAll();	
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
