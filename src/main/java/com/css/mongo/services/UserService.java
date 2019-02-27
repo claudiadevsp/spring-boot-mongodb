0a662ef08ec5b53a8468da45e81c853ae8a92bab
Claudia <claudia.devsp@gmail.com>
2019-02-27 09:57:05 -0300

    implements insert User

--- src/main/java/com/css/mongo/services/UserService.java
+++ src/main/java/com/css/mongo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.css.mongo.services;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.css.mongo.domain.User;
+import com.css.mongo.dto.UserDTO;
 import com.css.mongo.repository.UserRepository;
-import com.css.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -12,5 +14,13 @@ public class UserService {
 	private UserRepository repo;
 	public List<User> findAll() {
 		return repo.findAll();
-	}	
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getMail());
+	}
 }
